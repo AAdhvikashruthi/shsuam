7d84267f34a0d8cc46f1b11822400fdb6898dc49
amarcheluk <amarnathcs1995@gmail.com>
2023-12-01 17:23:59 +0530

    c1

---
+++ src/test/java/com/simple/amarTest.java
@@ -0,0 +1,11 @@
+package com.simple;
+
+import org.testng.annotations.Test;
+
+public class amarTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("swamiye sharanamayyappaaaaa");
+  }
+}
